3e93a49b2308914d0e390b575ea9d24dcc9b18b2
aleonal <jaleoncordero@miners.utep.edu>
2018-03-01 01:24:44 -0700

    Finalized header and code files for linked lists.

--- llist.c
+++ llist.c
@@ -39,13 +39,13 @@ void insertLinkedListNode(LList *list, char *stringToAdd) {
 }
 
 void removeLinkedListNode(LList *list, char *stringToDelete) {
-  LLNode *iterator = *list->first;
+  LLNode *iterator;
   LLNode *previous = *list->first;
   int length;
-  int isContained = 1;
+  int isContained = 2;
 
   /* Loops while there are nodes in the list or until the string to delete is found */
-  while(*iterator != 0) {
+  for(iterator = list->first; iterator; iterator = iterator->next) {
     /* Checks if the string in the current node is equal to the string we want to delete */
     for(length = 0; stringToDelete[length]; length++) {
       if(stringToDelete[length] != iterator->string[length]) {
@@ -54,19 +54,37 @@ void removeLinkedListNode(LList *list, char *stringToDelete) {
       }
     }
 
-    /* If the string is not equal, moves pointers down the list */
+    /* If the list is empty, displays message and ends function. If it is not, and the matching word is found, loop exits.
+       Otherwise, the pointers are moved down the list. */
     if(isContained == 0) {
       if(iterator != list->first)
 	previous = previous->next;
-      iterator = iterator->next;
-      isContained = 0;
-    } else
+      isContained = 2;
+    } else {
+      isContained = 1;
       break;
+    }
   }
+
+  if(isContained == 2) {
+    printf("User does not exist!\n");
+  }
+  else
+    printf("User was removed successfully!\n");
 
   /* Removes the node from the list */
   if(previous == list->first)             // If node to be removed is the head
     list->first = list->first->next;
   else                                   // If node to be removed is any other node
     previous->next = iterator->next;
+}
+
+void printLinkedList(LList *list) {
+  LLNode *iterator;
+  int count = 1;
+  printf("List contents: \n");
+  for(iterator = list->first; iterator; iterator = iterator->next) {
+    printf("  %d: <%s>\n", count iterator->string);
+    count++;
+  }
 }
